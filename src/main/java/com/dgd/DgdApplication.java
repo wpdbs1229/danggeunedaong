21b366f3bd8c1c91a65e98e45fee85836052ccc5
joony9393 <122251839+unbiz07806@users.noreply.github.com>
2023-06-05 13:51:43 +0900

    commit01

---
+++ src/main/java/com/dgd/DgdApplication.java
@@ -0,0 +1,13 @@
+package com.dgd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dgd/DgdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
